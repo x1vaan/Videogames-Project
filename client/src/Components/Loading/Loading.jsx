c0fc384039cd19996f1b9f6620a71450b78d9262
x1vaan <ivanx467@gmail.com>
2022-10-06 18:27:53 -0300

    FIX/ loading

--- client/src/Components/Loading/Loading.jsx
+++ client/src/Components/Loading/Loading.jsx
@@ -5,7 +5,6 @@ export default function Loading () {
     return (
         <div>
         <div className={Loadingcss.circle}></div>
-        <p className={Loadingcss.loading}>Loading...</p>
         </div>
     )
 }
