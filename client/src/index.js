ca3bf5b0abd6bf1e10ee968964113a243e5b2c00
x1vaan <ivanx467@gmail.com>
2022-12-21 19:12:43 -0300

    change back url deploy

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config()
 
-axios.defaults.baseURL = 'https://videogames-project-back-production.up.railway.app/';
+axios.defaults.baseURL = 'https://videogames-app-back.onrender.com/';
 
 ReactDOM.render(
   <React.StrictMode>
